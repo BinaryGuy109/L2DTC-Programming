264bc9bd7725c480bd19a81fce5ef260cb15f67d
BinaryGuy109 <102930955+BinaryGuy109@users.noreply.github.com>
2023-10-10 13:27:59 +1300

    Images / Sprites
    Made it so images show up, this has lead to some issues, like moving too fast and the snake won't turn, etc.

--- script.js
+++ script.js
@@ -1,56 +1,55 @@
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
+// const canvas = document.getElementById('gameCanvas');
+// const ctx = canvas.getContext('2d');
 
-const cellSize = 20;
-const gridSize = canvas.width / cellSize;
+// const cellSize = 20;
+// const gridSize = canvas.width / cellSize;
 
-let snake = [{ x: 5, y: 5 }];
-let direction = 'right';
+// let snake = [{ x: 5, y: 5 }];
+// let direction = 'right';
 
-function drawSnake() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    snake.forEach(segment => {
-        ctx.fillStyle = 'green';
-        ctx.fillRect(segment.x * cellSize, segment.y * cellSize, cellSize, cellSize);
-    });
-}
+// function drawSnake() {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     snake.forEach(segment => {
+//         ctx.fillStyle = 'green';
+//         ctx.fillRect(segment.x * cellSize, segment.y * cellSize, cellSize, cellSize);
+//     });
+// }
 
-function moveSnake() {
-    let headX = snake[0].x;
-    let headY = snake[0].y;
+// function moveSnake() {
+//     let headX = snake[0].x;
+//     let headY = snake[0].y;
 
-    if (direction === 'right') headX++;
-    if (direction === 'left') headX--;
-    if (direction === 'up') headY--;
-    if (direction === 'down') headY++;
+//     if (direction === 'right') headX++;
+//     if (direction === 'left') headX--;
+//     if (direction === 'up') headY--;
+//     if (direction === 'down') headY++;
 
-    snake.unshift({ x: headX, y: headY });
-    snake.pop();
-}
+//     snake.unshift({ x: headX, y: headY });
+//     snake.pop();
+// }
 
-function gameLoop() {
-    drawSnake();
-    moveSnake();
-    setTimeout(gameLoop, 200);
-}
+// function gameLoop() {
+//     drawSnake();
+//     moveSnake();
+//     setTimeout(gameLoop, 200);
+// }
 
-gameLoop();
+// gameLoop();
 
-document.addEventListener('keydown', event => {
-    switch (event.key) {
-        case 'ArrowUp':
-            if (direction !== 'down') direction = 'up';
-            break;
-        case 'ArrowDown':
-            if (direction !== 'up') direction = 'down';
-            break;
-        case 'ArrowLeft':
-            if (direction !== 'right') direction = 'left';
-            break;
-        case 'ArrowRight':
-            if (direction !== 'left') direction = 'right';
-            break;
-    }
-});
-
+// document.addEventListener('keydown', event => {
+//     switch (event.key) {
+//         case 'ArrowUp':
+//             if (direction !== 'down') direction = 'up';
+//             break;
+//         case 'ArrowDown':
+//             if (direction !== 'up') direction = 'down';
+//             break;
+//         case 'ArrowLeft':
+//             if (direction !== 'right') direction = 'left';
+//             break;
+//         case 'ArrowRight':
+//             if (direction !== 'left') direction = 'right';
+//             break;
+//     }
+// });
 
